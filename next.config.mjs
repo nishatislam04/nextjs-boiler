a258ec8b0ba287433c84c51d49488927659c2000
nishatislam04 <nishatislam3108@gmail.com>
2025-01-23 10:24:36 -0500

    save: & add redis

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true, // Enable SWC-based minification
   reactStrictMode: false,
-  experimental: {
-    turbo: true, // Activates Turbo Mode
-  },
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000, // Poll for changes every second
-      aggregateTimeout: 300, // Delay rebuild after changes
-    };
-  },
+  
   images: {
     remotePatterns: [
       {
