1a0a9e970cdb2faf7ad33011d4385bef512328dd
nishatislam04 <nishatislam3108@gmail.com>
2025-01-23 07:58:54 -0500

    test

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,5 @@
 import prisma from "@/prisma/db";
 
 export default async function Home() {
-	const users = await prisma.user.findMany();
-	console.log(users);
 	return <div className="">main page</div>;
 }
