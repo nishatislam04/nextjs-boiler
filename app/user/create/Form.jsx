49a0c997540377a7998f32ee41deaabed1cc5bc6
nishatislam04 <nishatislam3108@gmail.com>
2025-01-29 12:54:04 -0500

    add: form loading user create

--- app/user/create/Form.jsx
+++ app/user/create/Form.jsx
@@ -2,7 +2,13 @@
 import { useForm } from "@mantine/form";
 import { zodResolver } from "mantine-form-zod-resolver";
 import { createUser } from "@/lib/repository/actions/users/create";
-import { Button, PasswordInput, TextInput } from "@mantine/core";
+import {
+	Box,
+	Button,
+	LoadingOverlay,
+	PasswordInput,
+	TextInput,
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { IconLock } from "@tabler/icons-react";
 import { createUserSchema } from "@/lib/schema/user/create";
@@ -34,14 +40,17 @@ export default function CreateUserForm() {
 	});
 
 	const handleSubmit = async (values) => {
+		toggle();
 		setServerErrors({});
 
 		const response = await createUser(values);
 
 		if (!response.success) {
 			setServerErrors(response.errors);
+			toggle();
 			return;
 		} else {
+			toggle();
 			router.push("/user");
 		}
 	};
@@ -56,68 +65,77 @@ export default function CreateUserForm() {
 				radius: "md",
 			});
 			setServerErrors({});
+			toggle();
 		}
-	}, [serverErrors]);
+	}, [serverErrors, toggle]);
 
 	return (
-		<form
-			onSubmit={form.onSubmit(handleSubmit)}
-			className="rounded-lg bg-gray-100 px-3 py-6">
-			<Toast />
-			<div className="flex w-full gap-4">
-				<TextInput
-					className="mb-4 w-1/2"
-					label="Name"
-					size="xs"
-					name="name"
-					placeholder="Name"
-					key={form.key("name")}
-					{...form.getInputProps("name")}
-					error={form.errors.name || serverErrors.name}
-				/>
-				<TextInput
-					className="mb-4 w-1/2"
-					label="Email Address"
-					size="xs"
-					name="email"
-					placeholder="Email Address"
-					key={form.key("email")}
-					{...form.getInputProps("email")}
-					error={form.errors.email || serverErrors.email}
-				/>
-			</div>
-			<div className="flex w-full gap-4">
-				<TextInput
-					className="mb-4 w-1/2"
-					label="Userame"
-					size="xs"
-					name="username"
-					placeholder="Username"
-					key={form.key("username")}
-					{...form.getInputProps("username")}
-					error={form.errors.username || serverErrors.username}
-				/>
-				<PasswordInput
-					className="mb-4 w-1/2"
-					leftSection={passwordIcon}
-					label="password"
-					placeholder="Password"
-					visible={visible}
-					onVisibilityChange={toggle}
-					name="password"
-					key={form.key("password")}
-					{...form.getInputProps("password")}
-					error={form.errors.password || serverErrors.password}
-				/>
-			</div>
+		<Box pos="relative">
+			<LoadingOverlay
+				visible={visible}
+				zIndex={10000}
+				overlayProps={{ radius: "sm", blur: 2 }}
+			/>
+			<form
+				onSubmit={form.onSubmit(handleSubmit)}
+				className="rounded-lg bg-gray-100 px-3 py-6">
+				<Toast />
+				<div className="flex w-full gap-4">
+					<TextInput
+						className="mb-4 w-1/2"
+						label="Name"
+						size="xs"
+						name="name"
+						placeholder="Name"
+						key={form.key("name")}
+						{...form.getInputProps("name")}
+						error={form.errors.name || serverErrors.name}
+					/>
+					<TextInput
+						className="mb-4 w-1/2"
+						label="Email Address"
+						size="xs"
+						name="email"
+						placeholder="Email Address"
+						key={form.key("email")}
+						{...form.getInputProps("email")}
+						error={form.errors.email || serverErrors.email}
+					/>
+				</div>
+				<div className="flex w-full gap-4">
+					<TextInput
+						className="mb-4 w-1/2"
+						label="Userame"
+						size="xs"
+						name="username"
+						placeholder="Username"
+						key={form.key("username")}
+						{...form.getInputProps("username")}
+						error={form.errors.username || serverErrors.username}
+					/>
+					<PasswordInput
+						className="mb-4 w-1/2"
+						leftSection={passwordIcon}
+						label="password"
+						placeholder="Password"
+						visible={visible}
+						onVisibilityChange={toggle}
+						name="password"
+						key={form.key("password")}
+						{...form.getInputProps("password")}
+						error={form.errors.password || serverErrors.password}
+					/>
+				</div>
 
-			<Button
-				variant="filled"
-				size="xs"
-				radius="md"
-				type="submit">
-				Create
-			</Button>
-		</form>
+				<Button
+					loading={visible}
+					variant="filled"
+					size="xs"
+					radius="md"
+					type="submit">
+					Create
+				</Button>
+			</form>
+		</Box>
 	);
 }
